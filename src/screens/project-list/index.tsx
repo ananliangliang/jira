7035fcc1894c245a408049830b3767eec2623702
ananliangliang <ananliangliang@vip.qq.com>
2021-06-14 12:08:02 +0800

    chore: 添加ts类型定义

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -12,7 +12,7 @@ export const ProjectListScreen = () => {
         name: '',
         personId: ''
     })
-    const debounceParam = useDebounce(param, 2000)
+    const debounceParam = useDebounce(param, 200)
     const [list, setList] = useState([])
 
 
